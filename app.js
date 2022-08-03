c3614b6da5dbbf292f8bf57bb8d8af16a53986e4
zamorskimarek <zamorski.marek@gmail.com>
2022-08-03 23:02:50 +0200

    6

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-var person = {
-    name: "Max",
-    age: 30,
-    hobbies: ["sports", "cooking"],
-    role: [2, "author"]
-};
-// let favActiv: any[];
-// favActiv = ["sport", 1];
-person.role.push("admin");
-// person.role[1] = 10;
-console.log(person.role[1]);
-var favActiv;
-favActiv = ["sport"];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(input1, input2) {
+    //   console.log(typeof number1);
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinesNames = combine("max", "anna");
+console.log(combinesNames);
