dc01b5981c861ee0beda397e88f18b1ac282280d
zamorskimarek <zamorski.marek@gmail.com>
2022-08-10 23:33:25 +0200

    ts

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-function combine(input1, input2, resutlConversion) {
-    //   console.log(typeof number1);
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resutlConversion === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    //   if (resutlConversion === "as-number") {
-    //     return +result;
-    //     // or return parseFloat(result)
-    //   } else {
-    //     return result.toString();
-    //   }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-var combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-var combinesNames = combine("max", "anna", "as-text");
-console.log(combinesNames);
+function printResolve(num) {
+    console.log("Result:" + num);
+}
+printResolve(add(5, 12));
--- app.ts
+++ app.ts
@@ -1,3 +1,9 @@
-function add(n1: number, n2: number): number {
+function add(n1: number, n2: number) {
   return n1 + n2;
 }
+
+function printResolve(num: number) {
+  console.log("Result:" + num);
+}
+
+printResolve(add(5, 12));
