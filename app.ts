b4723523efa8fab2d676e7643e689a6cd2896b4f
zamorskimarek <zamorski.marek@gmail.com>
2022-08-10 23:37:07 +0200

    ts

--- app.ts
+++ app.ts
@@ -2,8 +2,10 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-function printResolve(num: number) {
+function printResolve(num: number): void {
   console.log("Result:" + num);
 }
 
 printResolve(add(5, 12));
+
+// let someVAl: undefined;
