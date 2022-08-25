2cfb836fdb50d4ce87804704f2c3516540935533
zamorskimarek <zamorski.marek@gmail.com>
2022-08-25 12:36:19 +0200

    ts

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-function printResolve(num) {
-    console.log("Result:" + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResolve(add(5, 12));
-// let combineValues: Function;
-var combineValues;
-combineValues = add;
-// combineValues = 5;
-// combineValues = printResolve;
-console.log(combineValues(8, 8));
-var someValue;
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+userName = userInput;
--- app.ts
+++ app.ts
@@ -1,29 +1,7 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
-}
+let userInput: any;
+let userName: string;
 
-function printResolve(num: number): void {
-  console.log("Result:" + num);
-}
+userInput = 5;
+userInput = "Max";
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResolve(add(5, 12));
-
-// let combineValues: Function;
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-// combineValues = 5;
-// combineValues = printResolve;
-
-console.log(combineValues(8, 8));
-
-let someValue: undefined;
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+userName = userInput;
