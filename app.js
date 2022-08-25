7c655679c7d0341cfca5c6e6b213a20c439eb984
zamorskimarek <zamorski.marek@gmail.com>
2022-08-25 12:31:18 +0200

    ts

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ function add(n1, n2) {
 function printResolve(num) {
     console.log("Result:" + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResolve(add(5, 12));
-// let combineValues: (a: number, b: number) => number;
-// combineValues = add;
+// let combineValues: Function;
+var combineValues;
+combineValues = add;
 // combineValues = 5;
 // combineValues = printResolve;
-// console.log(combineValues(8, 8));
-// let someVAl: undefined;
+console.log(combineValues(8, 8));
+var someValue;
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -6,15 +6,24 @@ function printResolve(num: number): void {
   console.log("Result:" + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResolve(add(5, 12));
 
-let combineValues: Function;
-// let combineValues: (a: number, b: number) => number;
+// let combineValues: Function;
+let combineValues: (a: number, b: number) => number;
 
-// combineValues = add;
+combineValues = add;
 // combineValues = 5;
 // combineValues = printResolve;
 
-// console.log(combineValues(8, 8));
+console.log(combineValues(8, 8));
 
 let someValue: undefined;
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
